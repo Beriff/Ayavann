5087b2552ea1c1393454f3d988d9e1d5a870d428
Beriff <58127431+Beriff@users.noreply.github.com>
2023-09-19 21:05:33 +0700

    Add ColorGradient struct

---
+++ Ayavann/Physics/ColorGradient.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ayavann.Physics
+{
+	struct ColorGradient
+	{
+		public Color Start;
+		public Color End;
+
+		public ColorGradient(Color start, Color end)
+		{
+			Start = start;
+			End = end;
+		}
+
+		public Color GetColor(float a)
+		{
+			Func<float, float, float, float> lerp = NumExtend.Lerp;
+			return new Color(lerp(Start.R, End.R, a), lerp(Start.G, End.G, a), lerp(Start.B, End.B, a));
+		}
+
+	}
+}
---
+++ Ayavann/World/Terrain/World.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ayavann.World.Terrain
+{
+	class World
+	{
+		private OctaveValueNoise WorldNoise;
+
+	}
+}
