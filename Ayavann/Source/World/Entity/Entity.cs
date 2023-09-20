f6c474da714e08f004b17587f6914821cbce5930
Froxwin <56168224+Froxwin@users.noreply.github.com>
2023-09-20 18:52:59 +0530

    uh um uhhh

--- Ayavann/Source/World/Entity/Entity.cs
+++ Ayavann/Source/World/Entity/Entity.cs
@@ -5,11 +5,17 @@ namespace Ayavann.World.Entity;
 
 class Entity
 {
-  public Model model;
+  public Model Model;
+  public Vector3 Position = new(0,0,0);
+
+  public Matrix Update(Matrix World) {
+    Position += new Vector3(0, 1f, 0);
+    return Matrix.CreateTranslation(Position);
+  }
 
   public void DrawModel(Matrix world, Matrix view, Matrix projection)
   {
-    foreach (ModelMesh mesh in model.Meshes)
+    foreach (ModelMesh mesh in Model.Meshes)
     {
       foreach (BasicEffect effect in mesh.Effects)
       {
